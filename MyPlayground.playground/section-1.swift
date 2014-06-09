f986631b212d8e0364168743a8b729283f40f869
Jholman8 <jholman8@kim244-152294.c.byui.edu>
2014-06-09 13:57:35 -0600

    Simple UIImageview in swift.
    Simple UIImageview in swift.

---
+++ MyPlayground.playground/section-1.swift
@@ -0,0 +1,11 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+
+var test =  UIImage(data: NSData(contentsOfURL: NSURL(string:"http://devhumor.com/wp-content/uploads/2012/04/devhumor.com_pointers.png")))
+let lbl = UILabel(frame: CGRectMake(0, 0, 300, 100))
+lbl.text = "Hello GitHub!!!"
