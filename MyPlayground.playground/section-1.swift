f488e56a88db780d86c7c9de1c7dd6529457ba15
Jholman8 <jholman8@kim244-152294.c.byui.edu>
2014-06-10 08:30:20 -0600

    Update to allow the image to load in an async call!

--- MyPlayground.playground/section-1.swift
+++ MyPlayground.playground/section-1.swift
@@ -5,7 +5,8 @@ import UIKit
 var str = "Hello, playground"
 
 
-
+dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
 var test =  UIImage(data: NSData(contentsOfURL: NSURL(string:"http://devhumor.com/wp-content/uploads/2012/04/devhumor.com_pointers.png")))
+})
 let lbl = UILabel(frame: CGRectMake(0, 0, 300, 100))
 lbl.text = "Hello GitHub!!!"
